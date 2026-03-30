015b728dcac2d5a96a9b0a4d207b87445304eecb
myoungsooy <myoungsooy@gmail.com>
2026-03-30 20:47:08 +0900

    연결강화 마지막

--- server.js
+++ server.js
@@ -12,21 +12,27 @@ app.use(express.static(path.join(__dirname, '.')));
 io.on('connection', (socket) => {
     console.log('유저 접속:', socket.id);
 
-    // 방 만들기/참가하기
     socket.on('join-room', (roomId) => {
         socket.join(roomId);
         console.log(`${socket.id}가 ${roomId}번 방에 입장`);
         
-        // 방에 2명이 모이면 게임 시작 신호 전송
         const clients = io.sockets.adapter.rooms.get(roomId);
         if (clients && clients.size === 2) {
-            io.to(roomId).emit('game-start');
+            // 두 명이 모이면 서버가 첫 번째 문제를 골라서 시작 신호를 보냄
+            const firstIdx = Math.floor(Math.random() * 50); 
+            io.to(roomId).emit('game-start', { firstIdx });
         }
     });
 
-    // 게임 데이터 중계 (데미지, 다음 문제 등)
     socket.on('game-data', (data) => {
-        socket.to(data.roomId).emit('peer-data', data);
+        if (data.type === 'req-next') {
+            // 한 명이라도 다음 문제를 요청하면 서버가 새 번호를 생성해 방 전체에 뿌림
+            const nextIdx = Math.floor(Math.random() * 50);
+            io.to(data.roomId).emit('peer-data', { type: 'next', idx: nextIdx });
+        } else {
+            // 결과(정답/오답) 데이터는 방 전체(나 포함)에 브로드캐스트하여 동시 연출
+            io.to(data.roomId).emit('peer-data', data);
+        }
     });
 
     socket.on('disconnect', () => {
